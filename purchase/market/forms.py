ef21a40fde95de9d6105157bbbdab0784745341c
Ananthasireesh.ta <ananthasireesh.ta@cilalabs.com>
2021-11-17 16:44:54 +0530

    purchase

---
+++ purchase/market/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
+
+class RegisterForm(FlaskForm):
+    """ Naming is very important for this flask always checks postceeder 
+        to validate and checks for the field with same name """
+    def validate_username(slef,username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('User Name already exists! Please try new UserName')
+
+    def validate_email_address(slef,email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('email_address already exists! Please try different Email_address')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
+
+class LoginForm(FlaskForm):
+      username = StringField(label='User Name:', validators=[DataRequired()])
+      password = PasswordField(label='Password:', validators=[DataRequired()])
+      submit = SubmitField(label='Sign In')
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item!')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item!')
